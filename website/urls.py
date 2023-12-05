0f852f463cdf2237a2e3b307283d4e55062e1638
sorenagorgan <hasanhasanhaji@gmail.com>
2023-12-05 19:05:40 +0330

    added static dir and urls
    + static root
    +static url
    +media url
    +media root

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from website.views import index_view, about_view, contact_view
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # path('url',view)
@@ -7,3 +9,6 @@ urlpatterns = [
     path('about/', about_view),
     path('contact/', contact_view)
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
