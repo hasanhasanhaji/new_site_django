a4b680fd356c69df9bf05be7eb3adb944ba4a780
sorenagorgan <hasanhasanhaji@gmail.com>
2023-12-22 17:36:38 +0330

    create post db
    create post model
    add it to admin

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
 class post(models.Model):
-    title = models.CharField
-    content = models.TextField
+    # image
+    # author
+    title = models.CharField(max_length=255, default='Default Title')
+    content = models.TextField()
+
+    # tag
+    # category
+
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now = True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
