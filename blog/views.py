3b4841671508097eb2ab6579bf671f371b7daf5b
sorenagorgan <hasanhasanhaji@gmail.com>
2023-12-24 13:43:58 +0330

    add 2 filter for post
    - counted_view
    - published posts

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from blog.models import post
+from django.utils import timezone
 # Create your views here.
 
 
 def blog_view(request):
-    posts = post.objects.filter(status = 1)
+    posts = post.objects.filter(status=1, published_date__lte=timezone.now())
+
+    # Update counted_views for each post
+    for p in posts:
+        p.counted_views += 1
+        p.save()
+
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
+
 
 def blog_single(request):
     return render(request,'blog/blog-single.html')
