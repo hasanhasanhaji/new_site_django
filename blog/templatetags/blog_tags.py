2895082bf3463b9555f25f0f8b97e02909437ef8
sorenagorgan <hasanhasanhaji@gmail.com>
2023-12-30 20:13:25 +0330

    some minor changes

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from blog.models import post
+
+register = template.Library()
+
+
+@register.simple_tag(name='totalposts')
+def function():
+    posts = post.objects.filter(status=1).count()
+    return posts
