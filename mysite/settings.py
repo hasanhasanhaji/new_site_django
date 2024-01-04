4342ea7827b4b8922db08b229d473dbf218b4805
sorenagorgan <hasanhasanhaji@gmail.com>
2024-01-04 12:35:02 +0330

    robots

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'robots',
     'website',
     'blog'
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', include('robots.urls'))
 
 ]
